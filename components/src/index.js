db404a93d993883203c73b79d5d96c26c6b7ccf5
erikmjohnson <erikarisma98@gmail.com>
2019-09-04 19:00:33 -0700

    Section 34

---
+++ components/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return(
+   <div className="ui container comments">
+    <CommentDetail author="Sam"/>
+    <CommentDetail author="Alex"/>
+    <CommentDetail auther="Jane"/>
+   </div>
+  )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
